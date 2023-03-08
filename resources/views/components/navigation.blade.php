a86096fe54d183bbb9ef012099d74d06adc6a0c6
igorprodev1 <igor.pro.dev1@gmail.com>
2023-03-08 22:41:07 +0200

    navigation refactoring

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,10 +1,18 @@
 <nav>
-  <div class="flex w-full gap-4 p-4 text-white bg-blue-900 justify-left">
-      <a href="/">{{ __('Home') }} </a>
-      <a href="/settings">{{ __('Settings') }} </a>
-      <a href="/parser">{{ __('Parser') }} </a>
-      <a href="/chat">{{ __('Chat') }} </a>
-      <a href="/login">{{ __('Login') }} </a>
-      <a href="/logout">{{ __('Logout') }} </a>
+  <div class="flex justify-between w-full p-4 text-white bg-blue-900">
+      <div class="flex justify-start gap-4">
+        <a href="/">{{ __('Home') }} </a>
+        <a href="/settings">{{ __('Settings') }} </a>
+        <a href="/parser">{{ __('Parser') }} </a>
+        <a href="/chat">{{ __('Chat') }} </a>
+      </div>
+
+      <div class="flex justify-start gap-4">
+        @if (auth()->check())
+          <a href="/logout">{{ __('Logout') }} </a>
+        @else
+          <a href="/login">{{ __('Login') }} </a>
+        @endif
+      </div>
   </div>
 </nav>
