6adcd5ee2d365a8cea6759a7cbfac81d3fef4ca8
igorprodev1 <igor.pro.dev1@gmail.com>
2023-03-08 22:52:32 +0200

    underline active menu item

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,10 +1,10 @@
 <nav>
   <div class="flex justify-between w-full p-4 text-white bg-blue-900">
       <div class="flex justify-start gap-4">
-        <a href="/">{{ __('Home') }} </a>
-        <a href="/settings">{{ __('Settings') }} </a>
-        <a href="/parser">{{ __('Parser') }} </a>
-        <a href="/chat">{{ __('Chat') }} </a>
+        <a href="/" class="{{ (request()->is('/')) ? 'underline' : '' }}">{{ __('Home') }} </a>
+        <a href="/settings" class="{{ (request()->is('settings')) ? 'underline' : '' }}">{{ __('Settings') }} </a>
+        <a href="/parser" class="{{ (request()->is('parser')) ? 'underline' : '' }}">{{ __('Parser') }} </a>
+        <a href="/chat" class="{{ (request()->is('chat')) ? 'underline' : '' }}">{{ __('Chat') }} </a>
       </div>
 
       <div class="flex justify-start gap-4">
